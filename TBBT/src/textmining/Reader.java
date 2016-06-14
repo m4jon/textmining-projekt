84f1f0fc8372e22c5f8917e6863660b3b7d3c799
Manuel Seeger <ms384@hdm-stuttgart.de>
2016-06-14 18:43:58 +0200

    fulltextBuilder

--- TBBT/src/textmining/Reader.java
+++ TBBT/src/textmining/Reader.java
@@ -9,6 +9,10 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -17,30 +21,41 @@ import java.io.File;
 public class Reader {
 
     public static void main(String[] args) throws IOException {
-        String target_dir = "/Users/Manuel-Mac/Desktop/txt";
+        String target_dir = "C:\\Users\\Zeisl\\Desktop\\Seasons";
         File dir = new File(target_dir);
         File[] files = dir.listFiles();
+        Map<String, Speaker> speaker = new HashMap<>();
+        StringBuilder fulltextBuilder = new StringBuilder();
 
         for (File f : files) {
             if(f.isFile()) {
-                BufferedReader inputStream = null;
-
-                try {
-                    inputStream = new BufferedReader(
-                                    new FileReader(f));
+                try (
+                        BufferedReader inputStream = new BufferedReader(
+                        new FileReader(f))) {
                     String line;
-
-                    while ((line = inputStream.readLine()) != null) {
+         
+                    while (null != (line = inputStream.readLine())) {
+                        fulltextBuilder.append(line);
                         System.out.println(line);
                     }
-                }
-                finally {
-                    if (inputStream != null) {
-                        inputStream.close();
+                    
+                    String fulltext = fulltextBuilder.toString();
+                    // StringBuffer Bereinigung = new StringBuffer();
+                    
+                    Pattern Klammern;
+                    Klammern = Pattern.compile(("[\\(\\w\\s\\W\\)]"));
+                    Matcher m = Klammern.matcher(fulltext);
+                    fulltext = m.replaceAll("");
+                    
+                    if (m.find()) {
+                        String tagname = m.group(1);
+                        if (tagname.startsWith("/(")) {
+                            tagname.replace("[\\(\\w\\s\\W\\)]", "");
+                        }
+                    
+                    
                     }
-                }
+                } 
             }
         }
     }
-
-}
