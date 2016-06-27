3d675847994a4e4b38d1b2fbfebb8b8ee4e09d62
Manuel Seeger <ms384@hdm-stuttgart.de>
2016-06-27 14:33:48 +0200

    Unötige Codezeilen entfernt

--- TBBT/src/textmining/Reader.java
+++ TBBT/src/textmining/Reader.java
@@ -1,9 +1,8 @@
-/* 
-* To change this license header, choose License Headers in Project Properties.
+/*
+ * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
 package textmining;
 
 import java.io.FileReader;
@@ -12,6 +11,8 @@ import java.io.IOException;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -33,16 +34,28 @@ public class Reader {
                    
                     
                    String line = inputStream.readLine();
+                   
                   
-                   while (line != null) {
+                   while ((line = inputStream.readLine()) != null) {
+                    
+                    //Löschen von leeren Zeilen
+                    String adjusted = line.replaceAll("(?m)^[ \t]*\r?\n", "");
+                    
+                    //Suche nach Klammern und deren Inhalt (Im Drehbuch Anweisungen) und ersetzte diese durch nichts.
+                    Pattern PatternKlammer = Pattern.compile ("[\\(\\w\\W\\)]");
+                    Matcher MatcherKlammer = PatternKlammer.matcher(line);
+                    line = MatcherKlammer.replaceAll("");     
+                    
+                    //Suche nach Zeilen mit Doppelpunkt - Speichere alles vor dem Doppelpunkt als Speaker, alles nach dem Doppelpunkt als Dialog
                     String[] parts = line.split(":");
                     String Speaker = parts[0];
-                    String Monolog;
-                       Monolog = parts[1];
+                    String Monolog = parts[1];
+                    
+                   
                    
                    speakers.put(Speaker, Monolog);
                    
-                   System.out.println(speakers);
+                   System.out.println(adjusted);
                    }
                       
                     }
