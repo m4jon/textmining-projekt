be9c0e3e4cd94308966dbb40ecf19316aadffe98
Manuel Seeger <ms384@hdm-stuttgart.de>
2016-06-28 18:08:16 +0200

    Aufteilung in mehrere Klassen

---
+++ TBBT/src/textmining/Parser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textmining;
+
+/**
+ *
+ * @author Manuel-Mac
+ */
+public class Parser {
+
+    // Übergabe des Strings "fulltext" aus der Reader-Klasse
+    private String fulltext;
+    public void setString(String inputString)
+    {
+        this.fulltext= inputString;
+    }
+}
--- TBBT/src/textmining/Reader.java
+++ TBBT/src/textmining/Reader.java
@@ -5,61 +5,42 @@
  */
 package textmining;
 
-import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
- * @author Manuel-Mac
+ * @author Zeisl
  */
 public class Reader {
-
+    
     public static void main(String[] args) throws IOException {
-        String target_dir = "/Users/Manuel-Mac/Documents/Projekte/txt";
+       // Erstellen  des Parser-Objekts für Übergabe
+       Parser TBBTParser = new Parser();
+       
+        
+       String target_dir = "/Users/Manuel-Mac/Documents/Projekte/txt";
         File dir = new File(target_dir);
         File[] files = dir.listFiles();
-        Map<String, String> speakers = new HashMap<>();
-        
 
         for (File f : files) {
             if(f.isFile()) {
                 try (BufferedReader inputStream = new BufferedReader(
                         new FileReader(f))) {
-                   
+                    String fulltext;
+
+                    while ((fulltext = inputStream.readLine()) != null) {
                     
-                   String line = inputStream.readLine();
-                   
-                  
-                   while ((line = inputStream.readLine()) != null) {
-                    
-                    //Löschen von leeren Zeilen
-                    String adjusted = line.replaceAll("(?m)^[ \t]*\r?\n", "");
-                    
-                    //Suche nach Klammern und deren Inhalt (Im Drehbuch Anweisungen) und ersetzte diese durch nichts.
-                    Pattern PatternKlammer = Pattern.compile ("[\\(\\w\\W\\)]");
-                    Matcher MatcherKlammer = PatternKlammer.matcher(line);
-                    adjusted = MatcherKlammer.replaceAll("");     
-                    
-                    //Suche nach Zeilen mit Doppelpunkt - Speichere alles vor dem Doppelpunkt als Speaker, alles nach dem Doppelpunkt als Dialog
-                    String[] parts = line.split(":");
-                    String Speaker = parts[0];
-                    String Monolog = parts[1];
-                    
-                   
-                   
-                   speakers.put(Speaker, Monolog);
-                   
-                   System.out.println(speakers);
-                   }
-                      
+                        // Übergabe an Klasse Parser
+                        TBBTParser.setString(fulltext);
+                                
+                     System.out.println(fulltext);
                     }
                 }
             }
         }
     }
+
+}
